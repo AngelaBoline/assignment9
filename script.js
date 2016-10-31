49efd221407293f5a6a446a1de38a83cd365d55d
AngelaBoline <angelaboline@gmail.com>
2016-10-30 19:16:42 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+ 
+ 	document.getElementById("main").innerHTML = "The document is ready now"
+	
+
+	//var newP = $("<p>");
+	//newP.append("The document is ready now");
+	//$("#main").html(newP);
+
+ 
+    });
